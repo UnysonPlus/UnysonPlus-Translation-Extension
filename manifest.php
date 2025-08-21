04b085dae34fd9c247be3e033e330ca435826956
jonmlas <jonmlas@gmail.com>
2025-08-21 19:05:51 +0800

    Manifest updated

--- manifest.php
+++ manifest.php
@@ -1,20 +1,29 @@
-<?php if ( ! defined( 'FW' ) ) {
-	die( 'Forbidden' );
-}
+<?php if (!defined('FW')) die('Forbidden');
 
 $manifest = array();
 
 $manifest['name']        = __( 'Translations', 'fw' );
-$manifest['description'] = __( 'This extension lets you translate your website in any language or even add multiple languages for your users to change at their will from the front-end.', 'fw' );
+$manifest['slug']        = 'unysonplus-translations';
+$manifest['description'] = __(
+	'This extension lets you translate your website in any language or even add multiple languages for your users to change at their will from the front-end.',
+	'fw'
+);
 
-$manifest['version'] = '1.0.10';
+$manifest['version']     = '1.0.10';
+$manifest['display']     = true;
+$manifest['standalone']  = true;
 
-$manifest['display'] = true;
-$manifest['standalone'] = true;
+// Repository Info
+$manifest['github_update'] = 'UnysonPlus/UnysonPlus-Translation-Extension';
+$manifest['github_repo']   = 'https://github.com/UnysonPlus/UnysonPlus-Translation-Extension';
+$manifest['github_branch'] = 'master';
 
-$manifest['github_update'] = 'ThemeFuse/Unyson-Translation-Extension';
+// Author Info
+$manifest['author']     = 'UnysonPlus';
+$manifest['author_uri'] = 'https://www.lastimosa.com.ph/unysonplus';
 
-$manifest['github_repo'] = 'https://github.com/ThemeFuse/Unyson-Translation-Extension';
-$manifest['uri'] = 'http://manual.unyson.io/en/latest/extension/translation/index.html#content';
-$manifest['author'] = 'ThemeFuse';
-$manifest['author_uri'] = 'http://themefuse.com/';
+// Meta
+$manifest['license']      = 'GPL-2.0-or-later';
+$manifest['text_domain']  = 'fw';
+$manifest['requires_php'] = '7.4';
+$manifest['requires_wp']  = '5.8';
